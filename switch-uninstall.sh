c59f49607553b9c8bb84b3bc5ea8b1e62320dca8
spantheslayer <supriyosarkar282@gmail.com>
2020-09-02 20:37:19 +0530

    uninstaller switch

--- switch-uninstall.sh
+++ switch-uninstall.sh
@@ -4,3 +4,34 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo 'Removing optimus-switch'
+
+
+
+rm -rf /usr/local/bin/set-amd.sh
+rm -rf /usr/local/bin/set-nvidia.sh
+rm -rf /usr/local/bin/optimus.sh
+rm -rf /usr/share/sddm/scripts/Xsetup
+rm -rf /etc/switch
+
+systemctl disable disable-nvidia.service
+rm -rf /etc/systemd/system/disable-nvidia.service
+
+rm -rf /etc/X11/mhwd.d/99-nvidia.conf
+rm -rf /etc/X11/xorg.conf.d/99-nvidia.conf
+rm -rf /etc/modprobe.d/99-nvidia.conf
+rm -rf /etc/modules-load.d/99-nvidia.conf
+
+rm -rf /etc/X11/xorg.conf.d/99-amd.conf
+rm -rf /etc/modprobe.d/99-amd.conf
+rm -rf /etc/modules-load.d/99-amd.conf
+
+sleep 1
+
+
+
+
+echo 'optimus-switch is now uninstalled'
+echo '########'
+echo 'if you reboot now, you will not have a working Xorg setup.'
+echo 'setup another optimus solution before rebooting.'
+rm -rf uninstall-switch.sh 
